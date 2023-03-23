e6a92e02d215c992c07abcd4e6e3d78744adc1cf
Vladyslav Yermolayev <v.yermolayev@voverc.com>
2023-03-23 19:29:00 +0200

    Init project

---
+++ README.md
@@ -0,0 +1,49 @@
+![logo](https://mhst.netlify.app/img/logo.png)
+## :pushpin: Starter HTML template
+#### :memo: Description: 
+
+Starter template for Front-End development. Flexible structure used for styles and gulp for automatization.
+___
+
+#### Includes
+
+- All Meta & Link Tags
+- HTML5 Tags & CSS3 Props
+- CSS & JavaScript Modules
+- Offline First Service Worker
+- manifest.json
+- AMP Project
+- All Security Headers
+- Express.js Server
+- netlify.toml
+- robots.txt
+- sitemap.xml
+- browserconfig.xml
+- .gitignore
+- README.md
+- Creative Error 404 Page
+- And More
+
+#### Usage
+1. ```bash
+npm install
+```
+
+2. ```bash
+gulp
+```
+
+#### :rocket: Technologies Used
+
+![HTML](https://img.shields.io/badge/html-e44d26.svg?&style=for-the-badge&logo=html5&logoColor=fff "HTML")&nbsp;
+![CSS](https://img.shields.io/badge/css-0692c1.svg?&style=for-the-badge&logo=css3&logoColor=fff "CSS")&nbsp;
+![SASS](https://img.shields.io/badge/sass-cf649a.svg?&style=for-the-badge&logo=sass&logoColor=fff "SASS")&nbsp;
+![Bootstrap](https://img.shields.io/badge/bootstrap-712cf9.svg?&style=for-the-badge&logo=bootstrap&logoColor=fff "Bootstrap")&nbsp;
+![BEM](https://img.shields.io/badge/bem-17a1e6.svg?&style=for-the-badge&logo=bem&logoColor=fff "BEM")&nbsp;
+![JavaScript](https://img.shields.io/badge/javascript-f7e018.svg?&style=for-the-badge&logo=javascript&logoColor=fff "JavaScript")&nbsp;
+![Gulp](https://img.shields.io/badge/gulp-cf4647.svg?&style=for-the-badge&logo=gulp&logoColor=fff "Gulp")&nbsp;
+___
+
+#### :envelope: Contacts
+* LinkedIn: [@vlad-yermolayev](https://www.linkedin.com/in/vlad-yermolayev/)
+* Telegram: [@vlad_yermolayev](https://t.me/vlad_yermolayev)
---
+++ gulpfile.js
@@ -0,0 +1,113 @@
+import gulp from 'gulp';
+import autoprefixer from 'gulp-autoprefixer';
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+const sass = gulpSass(dartSass);
+import sourcemaps from 'gulp-sourcemaps';
+import cssnano from 'gulp-cssnano';
+import uglify from 'gulp-uglify';
+import imagemin from 'gulp-imagemin';
+import rename from 'gulp-rename';
+import concat from 'gulp-concat';
+import del from 'del';
+import browsersync from 'browser-sync';
+
+let path = {
+    dist: {
+        html: './dist/',
+        css: './dist/css/',
+        js: './dist/js/',
+        img: './dist/img/',
+        fonts: './dist/fonts/',
+    },
+    src: {
+        html: './src/*.html',
+        style: './src/sass/*.scss',
+        js: './src/js/*.js',
+        img: './src/img/**/*.*',
+        fonts: './src/fonts/**/*.*',
+    },
+    watch: {
+        html: './src/**/*.html',
+        style: './src/sass/**/**/*.scss',
+        js: './src/js/**/*.js',
+        img: './src/img/**/*.*',
+        fonts: './src/fonts/**/*.*',
+    }
+};
+
+const clean = () => {
+    return del('dist');
+};
+
+const html = () => {
+    return gulp.src(path.src.html)
+        .pipe(gulp.dest(path.dist.html))
+        .pipe(browsersync.stream());
+}
+
+const styles = () => {
+    return gulp.src(path.src.style)
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({ cascade: false }))
+        .pipe(cssnano())
+        .pipe(rename('style.min.css'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.dist.css))
+        .pipe(browsersync.stream());
+}
+
+const scripts = () => {
+    return gulp.src(path.src.js)
+        .pipe(sourcemaps.init())
+        .pipe(concat('script.js'))
+        .pipe(uglify())
+        .pipe(rename('script.min.js'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.dist.js))
+        .pipe(browsersync.stream());
+}
+
+const images = () => {
+    return gulp.src(path.src.img)
+        .pipe(imagemin(
+            [
+                imagemin.mozjpeg({ quality: 75, progressive: true }),
+                imagemin.optipng({ optimizationLevel: 5 }),
+                imagemin.svgo({
+                    plugins: [
+                        { removeViewBox: true },
+                        { cleanupIDs: false }
+                    ]
+                })
+            ]
+        ))
+        .pipe(gulp.dest(path.dist.img))
+        .pipe(browsersync.stream());
+}
+
+const fonts = () => {
+    return gulp.src(path.src.fonts)
+        .pipe(gulp.dest(path.dist.fonts))
+        .pipe(browsersync.stream());
+}
+
+const browserSync = (done) => {
+    browsersync.init({
+        server: {
+            baseDir: './dist'
+        }
+    });
+    done();
+}
+
+const watchFiles = () => {
+    gulp.watch(path.src.fonts, gulp.series(fonts));
+    gulp.watch(path.src.style, gulp.series(styles));
+    gulp.watch(path.src.js, gulp.series(scripts));
+    gulp.watch(path.src.img, gulp.series(images));
+    gulp.watch(path.src.html, gulp.series(html));
+}
+
+export default gulp.series(clean, gulp.parallel(html, styles, scripts, images, fonts), gulp.parallel(watchFiles, browserSync));
